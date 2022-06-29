e50b3e02e884f2789a182bf9ac2bff412a12462f
lukpaikat <mhasbisaputra5@gmail.com>
2022-06-29 14:02:45 +0800

    remove unused initial files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '..src/App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/catatan-pelupa/i);
   expect(linkElement).toBeInTheDocument();
 });
