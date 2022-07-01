9063c5260abff72f53e14cc5ef7a0c069d75ee81
lukpaikat <mhasbisaputra5@gmail.com>
2022-07-01 14:07:48 +0800

    create index.jsx

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '..src/App';
-
-test('renders title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/catatan-pelupa/i);
-  expect(linkElement).toBeInTheDocument();
-});
