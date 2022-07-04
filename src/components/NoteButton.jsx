5248c110cf3f65ca38a2fb1ab2edf735fc108166
lukpaikat <mhasbisaputra5@gmail.com>
2022-07-04 15:42:50 +0800

    create note button

---
+++ src/components/NoteButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NoteButton({ onClick, text }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="rounded-lg border border-button-border-color py-2 px-4 hover:bg-button-hover-color focus:bg-button-hover-color active:bg-button-active-color"
+    >
+      {text}
+    </button>
+  );
+}
+
+NoteButton.propTypes = {
+  onClick: PropTypes.func,
+  text: PropTypes.string.isRequired,
+};
+
+NoteButton.defaultProps = {
+  // eslint-disable-next-line no-console
+  onClick: () => console.log('button clicked'),
+};
+
+export default NoteButton;
---
+++ src/components/NoteButtonRed.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NoteButtonRed({ onClick, text }) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="rounded-lg border border-button-border-color py-2 px-4 bg-button-red-bg-color hover:bg-button-red-hover-color focus:bg-button-red-hover-color active:bg-button-red-active-color"
+    >
+      {text}
+    </button>
+  );
+}
+
+NoteButtonRed.propTypes = {
+  onClick: PropTypes.func,
+  text: PropTypes.string.isRequired,
+};
+
+NoteButtonRed.defaultProps = {
+  // eslint-disable-next-line no-console
+  onClick: () => console.log('red button clicked'),
+};
+
+export default NoteButtonRed;
