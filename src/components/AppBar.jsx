2ef05dae844a95853e749e77077fd7eb6e699546
lukpaikat <mhasbisaputra5@gmail.com>
2022-07-04 14:50:47 +0800

    SearchBox toggle mechanism

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import SearchButton from './SearchButton';
+import SearchBox from './SearchBox';
 
 class AppBar extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = { isSearching: false };
-    this.onToggleSearch = this.onToggleSearch.bind(this);
+    this.state = { isShowSearchbox: false };
+    this.openSearchBox = this.openSearchBox.bind(this);
+    this.closeSearchBox = this.closeSearchBox.bind(this);
   }
 
-  onToggleSearch() {
-    this.setState((prevState) => !prevState);
+  openSearchBox() {
+    this.setState(() => ({ isShowSearchbox: true }));
+  }
+
+  closeSearchBox() {
+    this.setState(() => ({ isShowSearchbox: false }));
   }
 
   render() {
+    const { isShowSearchbox } = this.state;
+    const titleDisplayClass = isShowSearchbox ? 'hidden' : 'block';
     return (
-      <header className="px-3 py-1 w-full flex justify-between">
-        <h1>Catatan Pelupa</h1>
-        <SearchButton />
+      <header className="px-3 py-1 w-full flex items-center justify-between min-h-[44px]">
+        <h1 className={`text-lg font-bold text-white-text-color ${titleDisplayClass}`}>Catatan Pelupa</h1>
+        {isShowSearchbox
+          ? <SearchBox closeSearchBox={this.closeSearchBox} />
+          : <SearchButton onClick={this.openSearchBox} />}
       </header>
     );
   }
