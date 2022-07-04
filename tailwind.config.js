7ab428375ce0123885ce4b6bb272f6bd5f50695d
lukpaikat <mhasbisaputra5@gmail.com>
2022-07-04 14:47:45 +0800

    add default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-text-color': '#f5f5f5',
+        'black-background-color': '#252525',
+        'black-text-color': '#121212',
+        'orange-note-color': '#ffd179',
+        'red-note-color': '#FF7C81',
+        'green-note-color': '#C6D67D',
+        'blue-note-color': '#7AC3E6',
+        'purple-note-color': '#D986CC',
+      },
+    },
   },
   plugins: [],
 };
