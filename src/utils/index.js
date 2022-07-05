13834704ef8ce8607a0626e9c8519187641b4d61
lukpaikat <mhasbisaputra5@gmail.com>
2022-07-05 14:33:28 +0800

    add color property to data

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,7 @@ const getInitialData = () => ([
     body: 'Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.',
     createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
+    color: 'red',
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ const getInitialData = () => ([
     body: 'Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.',
     createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
+    color: 'orange',
   },
   {
     id: 3,
@@ -19,6 +21,7 @@ const getInitialData = () => ([
     body: 'Dalam konteks pemrograman JavaScript, modularization merupakan teknik dalam memecah atau menggunakan kode dalam berkas JavaScript secara terpisah berdasarkan tanggung jawabnya masing-masing.',
     createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
+    color: 'green',
   },
   {
     id: 4,
@@ -26,6 +29,7 @@ const getInitialData = () => ([
     body: 'Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ',
     createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
+    color: 'blue',
   },
   {
     id: 5,
@@ -33,6 +37,7 @@ const getInitialData = () => ([
     body: 'ESM (ECMAScript Module) merupakan format modularisasi standar JavaScript.',
     createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
+    color: 'purple',
   },
   {
     id: 6,
@@ -40,6 +45,7 @@ const getInitialData = () => ([
     body: 'Dalam konteks pemrograman JavaScript, module bundler merupakan tools yang digunakan untuk menggabungkan seluruh modul JavaScript yang digunakan oleh aplikasi menjadi satu berkas.',
     createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
+    color: 'red',
   },
 ]);
 
@@ -53,4 +59,16 @@ const showFormattedDate = (date) => {
   return new Date(date).toLocaleDateString('id-ID', options);
 };
 
-export { getInitialData, showFormattedDate };
+const getNoteColorClassName = (color) => {
+  const noteColorClassName = {
+    orange: 'bg-orange-note-color',
+    red: 'bg-red-note-color',
+    green: 'bg-green-note-color',
+    blue: 'bg-blue-note-color',
+    purple: 'bg-purple-note-color',
+  };
+
+  return noteColorClassName[color];
+};
+
+export { getInitialData, showFormattedDate, getNoteColorClassName };
