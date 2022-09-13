6341da55fa0278ea2a1d52c2d8b3434f38086565
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-13 10:49:27 +0800

    remove color select component set form color to random

--- src/components/NewNoteForm.jsx
+++ src/components/NewNoteForm.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SubmitButton from './SubmitButton';
-import ColorSelect from './ColorSelect';
-import { getNoteColorClassName } from '../utils';
+import getNoteColorClassName from '../utils/getNoteColorClassName';
 
 class NewNoteForm extends React.Component {
   constructor(props) {
@@ -58,16 +57,15 @@ class NewNoteForm extends React.Component {
   }
 
   render() {
-    const { title, body, color } = this.state;
+    const { title, body } = this.state;
     return (
-      <form className={`py-2 px-6 w-full max-w-lg 2xl:max-w-2xl ${getNoteColorClassName(color)} mx-auto rounded-lg transition-all duration-150`} onSubmit={this.onSubmit}>
+      <form className={`py-2 px-6 w-full max-w-lg 2xl:max-w-2xl ${getNoteColorClassName() || 'bg-orange-note-color'} mx-auto rounded-lg transition-all duration-150`} onSubmit={this.onSubmit}>
         <label htmlFor="judulCatatan">
           <span className="sr-only">Judul Catatan</span>
           <input className="block placeholder-black placeholder-opacity-50 bg-transparent border border-button-border-color my-2 w-full rounded-lg p-2 text-black-text-color 2xl:text-xl" type="text" id="judulCatatan" value={title} maxLength="50" onChange={this.onTitleChange} placeholder="Judul Catatan" required />
         </label>
         <textarea className="block w-full my-2 border border-button-border-color bg-transparent p-2 placeholder-black placeholder-opacity-50 rounded-lg 2xl:text-xl" rows="8" placeholder="tulis catatan disini" onChange={this.onBodyChange} value={body} />
-        <div className="flex justify-between">
-          <ColorSelect value={color} onChange={this.onColorChange} />
+        <div className="flex justify-end">
           <SubmitButton text="Submit" />
         </div>
       </form>
