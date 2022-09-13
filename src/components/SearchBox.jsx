4e290c445e7abcae65a4ffae8776f50a4f59383f
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-14 07:49:18 +0800

    update searchbox functions
    make searchbox recieve and update data from it's parent component

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,44 +3,22 @@ import PropTypes from 'prop-types';
 import CloseSearchButton from './CloseSearchButton';
 import 'animate.css';
 
-class SearchBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { searchKey: '' };
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onCloseHandler = this.onCloseHandler.bind(this);
-  }
-
-  onChangeHandler(event) {
-    const { onSearchNote } = this.props;
-    const { value } = event.target;
-    this.setState(() => ({ searchKey: value }));
-    onSearchNote(value);
-  }
-
-  onCloseHandler() {
-    const { onSearchNote } = this.props;
-    this.setState(() => ({ searchKey: '' }));
-    onSearchNote('');
-  }
-
-  render() {
-    const { searchKey } = this.state;
-
-    return (
-      <form className="flex items-center w-full justify-end animate__animated animate__fadeIn">
-        <label htmlFor="searchBox" className="w-full sm:max-w-xs">
-          <span className="sr-only">SearchBox</span>
-          <input type="text" id="searchBox" placeholder="cari" onChange={this.onChangeHandler} value={searchKey} className="2xl:text-lg block w-full bg-white-text-color min-h-[44px] pl-2 rounded-l-lg" />
-        </label>
-        <CloseSearchButton onClick={this.onCloseHandler} />
-      </form>
-    );
-  }
+function SearchBox({ keyword, keywordChange, clearKeyword }) {
+  return (
+    <div className="flex items-center w-full justify-end animate__animated animate__fadeIn md:mb-2">
+      <label htmlFor="searchBox" className="w-full">
+        <span className="sr-only">SearchBox</span>
+        <input type="text" id="searchBox" placeholder="cari" onChange={(event) => keywordChange(event.target.value)} value={keyword} className="2xl:text-lg block w-full bg-white-text-color min-h-[44px] pl-2 rounded-l-lg" />
+      </label>
+      <CloseSearchButton onClick={clearKeyword} />
+    </div>
+  );
 }
 
 SearchBox.propTypes = {
-  onSearchNote: PropTypes.func.isRequired,
+  keyword: PropTypes.string.isRequired,
+  keywordChange: PropTypes.func.isRequired,
+  clearKeyword: PropTypes.func.isRequired,
 };
 
 export default SearchBox;
