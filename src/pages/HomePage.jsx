263883a65ec821dee0964023bbcbdbb8d6a4c72a
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-14 07:46:30 +0800

    make HomePage searchable and working card buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import NoteList from '../components/NoteList';
 import { getActiveNotes, archiveNote, deleteNote } from '../utils/local-data';
+import filterNotes from '../utils/filterNotes';
+import SearchBox from '../components/SearchBox';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -10,20 +12,13 @@ class HomePage extends React.Component {
       notes: getActiveNotes(),
       keyword: '',
     };
-    this.handleDeleteNote = this.handleDeleteNote.bind(this);
-    this.handleArchiveNote = this.handleArchiveNote.bind(this);
+    this.deleteNoteHandler = this.deleteNoteHandler.bind(this);
+    this.archiveNoteHandler = this.archiveNoteHandler.bind(this);
+    this.keywordChangeHandler = this.keywordChangeHandler.bind(this);
+    this.clearKeywordHandler = this.clearKeywordHandler.bind(this);
   }
 
-  handleDeleteNote(id) {
-    deleteNote(id);
-    this.setState(() => (
-      {
-        notes: getActiveNotes(),
-      }
-    ));
-  }
-
-  handleArchiveNote(id) {
+  archiveNoteHandler(id) {
     archiveNote(id);
     this.setState(() => (
       {
@@ -32,16 +27,47 @@ class HomePage extends React.Component {
     ));
   }
 
+  deleteNoteHandler(id) {
+    deleteNote(id);
+    this.setState(() => (
+      {
+        notes: getActiveNotes(),
+      }
+    ));
+  }
+
+  keywordChangeHandler(keyword) {
+    this.setState(() => (
+      {
+        keyword,
+      }
+    ));
+  }
+
+  clearKeywordHandler() {
+    this.setState(() => (
+      {
+        keyword: '',
+      }
+    ));
+  }
+
   render() {
-    const { notes } = this.state;
+    const { notes, keyword } = this.state;
+    const filteredNotes = filterNotes(notes, keyword);
+
     return (
       <>
         <h2 className="text-lg 2xl:text-3xl font-bold text-white-text-color my-6">Catatan Aktif</h2>
-        {/* search Bar */}
+        <SearchBox
+          keyword={keyword}
+          clearKeyword={this.clearKeywordHandler}
+          keywordChange={this.keywordChangeHandler}
+        />
         <NoteList
-          notes={notes}
-          onMoveNote={this.handleDeleteNote}
-          onDeleteNote={this.handleArchiveNote}
+          notes={filteredNotes}
+          onMoveNote={this.archiveNoteHandler}
+          onDeleteNote={this.deleteNoteHandler}
         />
       </>
     );
