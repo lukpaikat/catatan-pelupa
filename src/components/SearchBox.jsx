71fd9099b05e86057d098e17c1a8a1c02536e30a
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-14 18:04:01 +0800

    style mb-6 for searchbox container

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,7 +4,7 @@ import CloseSearchButton from './CloseSearchButton';
 
 function SearchBox({ keyword, keywordChange, clearKeyword }) {
   return (
-    <div className="flex items-center w-full justify-end md:mb-2">
+    <div className="flex items-center w-full justify-end mb-6">
       <label htmlFor="searchBox" className="w-full">
         <span className="sr-only">SearchBox</span>
         <input type="text" id="searchBox" placeholder="cari" onChange={(event) => keywordChange(event.target.value)} value={keyword} className="text-white-text-color 2xl:text-lg block w-full bg-gray-600 min-h-[44px] pl-2 rounded-l-lg" />
