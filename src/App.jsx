7c45041a68c55058a12f20894bfa04346b5174ef
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-14 09:10:50 +0800

    add archive page to route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppBar from './components/AppBar';
 import HomePage from './pages/HomePage';
+import ArchivePage from './pages/ArchivePage';
 import Page404 from './pages/Page404';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main className="px-2">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/archive" element={<ArchivePage />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </main>
