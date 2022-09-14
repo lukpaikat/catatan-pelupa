11d079c53f43ce6b826a98e881a64b70da7e165b
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-14 18:00:49 +0800

    style: change searchbox Background to grey

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -7,7 +7,7 @@ function SearchBox({ keyword, keywordChange, clearKeyword }) {
     <div className="flex items-center w-full justify-end md:mb-2">
       <label htmlFor="searchBox" className="w-full">
         <span className="sr-only">SearchBox</span>
-        <input type="text" id="searchBox" placeholder="cari" onChange={(event) => keywordChange(event.target.value)} value={keyword} className="2xl:text-lg block w-full bg-white-text-color min-h-[44px] pl-2 rounded-l-lg" />
+        <input type="text" id="searchBox" placeholder="cari" onChange={(event) => keywordChange(event.target.value)} value={keyword} className="text-white-text-color 2xl:text-lg block w-full bg-gray-600 min-h-[44px] pl-2 rounded-l-lg" />
       </label>
       <CloseSearchButton onClick={clearKeyword} />
     </div>
