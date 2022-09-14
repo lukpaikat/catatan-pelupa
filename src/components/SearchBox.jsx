5e83f75d05e3154e980d687c1041ea64d3a9f21b
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-14 09:09:19 +0800

    remove searchbox appear animation

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CloseSearchButton from './CloseSearchButton';
-import 'animate.css';
 
 function SearchBox({ keyword, keywordChange, clearKeyword }) {
   return (
-    <div className="flex items-center w-full justify-end animate__animated animate__fadeIn md:mb-2">
+    <div className="flex items-center w-full justify-end md:mb-2">
       <label htmlFor="searchBox" className="w-full">
         <span className="sr-only">SearchBox</span>
         <input type="text" id="searchBox" placeholder="cari" onChange={(event) => keywordChange(event.target.value)} value={keyword} className="2xl:text-lg block w-full bg-white-text-color min-h-[44px] pl-2 rounded-l-lg" />
