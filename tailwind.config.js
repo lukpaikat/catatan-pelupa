6fc9f568677f472be4e120ed86d3bfd9ee8083f8
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-14 16:43:15 +0800

    install tailwind line clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -21,5 +22,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 };
