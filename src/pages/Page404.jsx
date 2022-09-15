e2b81bad5490446b53c134c6b81c10b39e19fa1a
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-15 16:23:05 +0800

    animate illustration entrance

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// import cat404 from '../assets/cat404.svg';
 import pageNotFoundIllustration from '../assets/undraw_page_not_found_green.svg';
+import 'animate.css';
 
 function Page404() {
   return (
     <>
-      <img className="w-[80%] max-w-sm md:max-w-md mx-auto mt-[10vh]" src={pageNotFoundIllustration} alt="404 mountain" />
+      <img className="w-[80%] max-w-sm md:max-w-md mx-auto mt-[10vh] animate__animated animate__fadeIn" src={pageNotFoundIllustration} alt="404 mountain" />
       <p className="text-white-text-color text-xl text-center mt-7 mb-[20vh]">Halaman yang kamu cari tidak ditemukan</p>
     </>
   );
