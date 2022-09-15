c584980b0a79b3ef637b93aff67a2984cfd8ef8f
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-15 09:35:35 +0800

    create active link indicator

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BiHomeAlt, BiArchive } from 'react-icons/bi';
+import { NavLink } from 'react-router-dom';
+import { House, Archive } from 'phosphor-react';
 
 function AppBar() {
+  const NavLinkActiveClass = (isActive) => {
+    const defaultNavLinkClass = 'hover:bg-gray-600 focus:bg-gray-600 active:bg-gray-400 rounded-md min-h-[44px] min-w-[44px] flex';
+    const activeClass = isActive ? 'bg-gray-600' : 'bg-transparent';
+
+    return `${activeClass} ${defaultNavLinkClass}`;
+  };
+
   return (
     <header className="bg-black-background-color px-3 py-1 w-full flex items-center justify-between min-h-[44px] sticky z-10 top-0">
       <h1 className="my-2 text-xl 2xl:text-4xl font-bold text-white-text-color block">Catatan Pelupa</h1>
       <nav className="flex gap-3">
-        <Link to="/"><BiHomeAlt className="text-4xl text-white-text-color" /></Link>
-        <Link to="/archive"><BiArchive className="text-4xl text-white-text-color" /></Link>
+        <NavLink className={({ isActive }) => NavLinkActiveClass(isActive)} to="/">
+          <House className="text-3xl text-white-text-color m-auto" weight="light" />
+        </NavLink>
+        <NavLink className={({ isActive }) => NavLinkActiveClass(isActive)} to="/archive">
+          <Archive className="text-3xl text-white-text-color m-auto" />
+        </NavLink>
       </nav>
     </header>
   );
