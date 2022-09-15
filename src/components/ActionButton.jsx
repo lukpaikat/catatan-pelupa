311a70bdd016db21c4c10198769439b79ccdae23
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-15 09:36:03 +0800

    change to a slimmer icons

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -4,20 +4,20 @@ import { FloppyDisk, Plus } from 'phosphor-react';
 
 function ActionButton({ title, onClick }) {
   const buttonIcons = {
-    save: <FloppyDisk className="m-auto text-3xl text-white-text-color" />,
-    add: <Plus className="m-auto text-3xl text-white-text-color" />,
+    save: <FloppyDisk className="m-auto text-3xl text-white-text-color" weight="light" />,
+    add: <Plus className="m-auto text-3xl text-white-text-color" weight="light" />,
   };
 
   const buttonBackgrounds = {
-    save: 'bg-sky-600',
-    add: 'bg-gray-600',
+    save: 'bg-sky-600 hover:bg-sky-400 focus:bg-sky-400 active:bg-sky-500',
+    add: 'bg-gray-600 hover:bg-gray-400 focus:bg-gray-400 active:bg-gray-500',
   };
 
   return (
     <button
       type="button"
       onClick={onClick}
-      className={`h-[44px] w-[44px] drop-shadow-lg rounded-lg ${buttonBackgrounds[title]}`}
+      className={`h-[44px] w-[44px] drop-shadow-lg rounded-lg ${buttonBackgrounds[title]} active:translate-y-1 active:drop-shadow-md`}
     >
       {buttonIcons[title]}
     </button>
