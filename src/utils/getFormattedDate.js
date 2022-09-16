63b38702187e845ff03a86574dd6488f4383a246
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-16 10:30:42 +0800

    create getFormattedDate

---
+++ src/utils/getFormattedDate.js
@@ -0,0 +1,11 @@
+const getFormattedDate = (date) => {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return new Date(date).toLocaleDateString('id-ID', options);
+};
+
+export default getFormattedDate;
