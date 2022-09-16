f9546b65e17ca59d1f95c59ad9c1bccc900a689c
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-16 10:29:39 +0800

    make 404 page routeabel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import AppBar from './components/AppBar';
 import HomePage from './pages/HomePage';
 import ArchivePage from './pages/ArchivePage';
 import Page404 from './pages/Page404';
 import NewNotePage from './pages/NewNotePage';
+import NoteDetailPage from './pages/NoteDetailPage';
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/archive" element={<ArchivePage />} />
           <Route path="/notes/new" element={<NewNotePage />} />
-          <Route path="*" element={<Page404 />} />
+          <Route path="/notes/:id" element={<NoteDetailPage />} />
+          <Route path="/404page" element={<Page404 />} />
+          <Route path="*" element={<Navigate to="/404page" />} />
         </Routes>
       </main>
     </>
