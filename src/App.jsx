d139a3ff99dd139bd2c4e8e6a5242cf0992dbcb4
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-20 11:55:02 +0800

    create paths configuration file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  HOME, ARCHIVE, NOTES_NEW, NOTES_DETAIL, PAGE404,
+} from './config/paths';
 import AppBar from './components/AppBar';
 import HomePage from './pages/HomePage';
 import ArchivePage from './pages/ArchivePage';
@@ -13,12 +16,12 @@ function App() {
       <AppBar />
       <main className="px-2">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/archive" element={<ArchivePage />} />
-          <Route path="/notes/new" element={<NewNotePage />} />
-          <Route path="/notes/:id" element={<NoteDetailPage />} />
-          <Route path="/404page" element={<Page404 />} />
-          <Route path="*" element={<Navigate to="/404page" />} />
+          <Route path={HOME} element={<HomePage />} />
+          <Route path={ARCHIVE} element={<ArchivePage />} />
+          <Route path={NOTES_NEW} element={<NewNotePage />} />
+          <Route path={NOTES_DETAIL} element={<NoteDetailPage />} />
+          <Route path={PAGE404} element={<Page404 />} />
+          <Route path="*" element={<Navigate to={PAGE404} />} />
         </Routes>
       </main>
     </>
---
+++ src/config/paths.js
@@ -0,0 +1,13 @@
+const HOME = '/';
+const ARCHIVE = '/archive';
+const NOTES_NEW = '/notes/new';
+const NOTES_DETAIL = '/notes/:id';
+const PAGE404 = '/404page';
+
+export {
+  HOME,
+  ARCHIVE,
+  NOTES_NEW,
+  NOTES_DETAIL,
+  PAGE404,
+};
