871ff71eb74be216299a1d6777e2ac9aca22a132
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-22 13:22:05 +0800

    create NotePaperLarge component
    this component is used as a container for NoteDetails or NewNote form

---
+++ src/components/NotePaperLarge.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import getNoteColorClassName from '../utils/getNoteColorClassName';
+
+function NotePaperLarge({ children, noteTitle }) {
+  return (
+    <section
+      className={`py-2 px-6 w-full max-w-[700px] lg:max-w-[1000px] 2xl:max-w-[1300px]
+      ${getNoteColorClassName(noteTitle) || 'bg-orange-note-color'} 
+      mx-auto rounded-lg transition-all duration-150 min-h-[92vh] h-fit`}
+    >
+      {children}
+    </section>
+  );
+}
+
+NotePaperLarge.propTypes = {
+  children: PropTypes.node.isRequired,
+  noteTitle: PropTypes.string,
+};
+
+NotePaperLarge.defaultProps = {
+  noteTitle: 'ini judul placeholder',
+};
+
+export default NotePaperLarge;
