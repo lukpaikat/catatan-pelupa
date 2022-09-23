327960f7ac1cf222633abd8047473a0de55bf94d
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 07:49:20 +0800

    make 404page light and dark mode

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import pageNotFoundIllustration from '../assets/undraw_page_not_found_green.svg';
-
+import pageNotFoundIllustratioGreen from '../assets/undraw_page_not_found_green.svg';
+import pageNotFoundIllustrationBlack from '../assets/undraw_page_not_found_black.svg';
+import ThemeContext from '../contexts/ThemeContext';
+// TODO: make it adaptable to ThemeContext
 function Page404() {
+  const { theme } = React.useContext(ThemeContext);
+
+  const pageNotFoundIllustration = theme === 'light' ? pageNotFoundIllustrationBlack : pageNotFoundIllustratioGreen;
+
   return (
     <>
-      <img className="w-[80%] max-w-sm md:max-w-md mx-auto mt-[10vh] animate__animated animate__fadeIn" src={pageNotFoundIllustration} alt="404 mountain" />
-      <p className="text-white-text-color text-xl text-center mt-7 mb-[20vh]">Halaman yang kamu cari tidak ditemukan</p>
+      <img className="w-[80%] max-w-sm md:max-w-md mx-auto mt-[10vh] animate__animated animate__fadeIn" src={pageNotFoundIllustration} alt="404" />
+      <p className="text-gray-text-color semi-and-dark:text-white-text-color text-xl text-center mt-7 mb-[20vh]">Halaman yang kamu cari tidak ditemukan</p>
     </>
   );
 }
