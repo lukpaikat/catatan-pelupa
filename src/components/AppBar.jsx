b9adae080da24c52577047929431fc44c73d2bbc
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-23 09:55:34 +0800

    create theme menu state

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,8 +3,15 @@ import { NavLink } from 'react-router-dom';
 import { PushPin, Archive } from 'phosphor-react';
 import { HOME, ARCHIVE } from '../config/paths';
 import ThemeButton from './buttons/ThemeButton';
+import ThemeMenuContainer from './ThemeMenuContainer';
 
 function AppBar() {
+  const [isThemeMenuHidden, setIsThemeMenuHidden] = React.useState(true);
+
+  const themeMenuHandler = () => {
+    setIsThemeMenuHidden((prevState) => !prevState);
+  };
+
   const NavLinkActiveClass = (isActive) => {
     const defaultNavLinkClass = 'hover:bg-gray-600 focus:bg-gray-600 active:bg-gray-400 rounded-md min-h-[44px] min-w-[44px] flex transition-colors duration-300';
     const activeClass = isActive ? 'bg-gray-600' : 'bg-transparent';
@@ -15,7 +22,7 @@ function AppBar() {
   return (
     <header className="bg-black-background-color px-3 py-1 w-full flex items-center justify-between min-h-[44px] sticky z-10 top-0">
       <h1 className="my-2 text-xl 2xl:text-4xl font-bold text-white-text-color block">Catatan Pelupa</h1>
-      <div className="flex gap-3">
+      <div className="flex gap-3 relative">
         <nav className="flex gap-3">
           <NavLink className={({ isActive }) => NavLinkActiveClass(isActive)} to={HOME} end>
             <PushPin className="text-3xl text-white-text-color m-auto" weight="light" />
@@ -24,7 +31,11 @@ function AppBar() {
             <Archive className="text-3xl text-white-text-color m-auto" />
           </NavLink>
         </nav>
-        <ThemeButton />
+        <ThemeButton onClick={themeMenuHandler} />
+        <ThemeMenuContainer
+          themeMenuHandler={themeMenuHandler}
+          isThemeMenuHidden={isThemeMenuHidden}
+        />
       </div>
     </header>
   );
