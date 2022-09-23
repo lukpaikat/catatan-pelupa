05f23a76dbe46dfb0ab0ab6ac0cb4afe746f7791
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-23 17:43:42 +0800

    fix not full ThemeMenuButton

--- src/components/buttons/ThemeItemButton.jsx
+++ src/components/buttons/ThemeItemButton.jsx
@@ -4,14 +4,12 @@ import { Moon, Sun, SunHorizon } from 'phosphor-react';
 import camelCaseStr from '../../utils/camelCaseStr';
 
 function ThemeMenuButton({ title, currentTheme, setCurrentTheme }) {
-  // TODO: mungkin pindah ke container supaya tidak memanggil use context
-  // berkali-kali
   const camelCasedTitle = camelCaseStr(title);
 
   const opacityClass = currentTheme === camelCasedTitle ? 'opacity-100' : 'opacity-25 hover:opacity-90 focus:opacity-90';
 
   return (
-    <button onClick={setCurrentTheme} type="button" className={`flex gap-2 p-2 min-w-[150px] hover:bg-gray-600 rounded-lg ${opacityClass}`}>
+    <button onClick={setCurrentTheme} type="button" className={`flex gap-2 p-2 w-full hover:bg-gray-600 rounded-lg ${opacityClass}`}>
       {title === 'Dark' && <Moon className="text-3xl text-white-text-color" />}
       {title === 'Light' && <Sun className="text-3xl text-white-text-color" />}
       {title === 'Semi Dark' && <SunHorizon className="text-3xl text-white-text-color" />}
