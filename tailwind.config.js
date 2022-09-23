6c020a9ddf50c092668bdfef27729aa456eae957
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-23 10:22:54 +0800

    set tailwind darkmode to manual

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
