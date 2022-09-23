a925b497e20f3315a44c121fccb3d38c2f7004fd
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-23 09:53:21 +0800

    add string camel caser utility

---
+++ src/utils/camelCaseStr.js
@@ -0,0 +1,3 @@
+const camelCaseStr = (str) => str.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => (index === 0 ? word.toLowerCase() : word.toUpperCase())).replace(/\s+/g, '');
+
+export default camelCaseStr;
