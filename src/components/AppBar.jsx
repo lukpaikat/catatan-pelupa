e8182234927fbd1ed9b11454b1a84c15b9ad5703
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-23 13:54:30 +0800

    make app bar themeable

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { PushPin, Archive } from 'phosphor-react';
 import { HOME, ARCHIVE } from '../config/paths';
 import ThemeButton from './buttons/ThemeButton';
 import ThemeMenuContainer from './ThemeMenuContainer';
+import NavLinkButton from './buttons/NavLinkButton';
 
 function AppBar() {
   const [isThemeMenuHidden, setIsThemeMenuHidden] = React.useState(true);
@@ -12,24 +12,17 @@ function AppBar() {
     setIsThemeMenuHidden((prevState) => !prevState);
   };
 
-  const NavLinkActiveClass = (isActive) => {
-    const defaultNavLinkClass = 'hover:bg-gray-600 focus:bg-gray-600 active:bg-gray-400 rounded-md min-h-[44px] min-w-[44px] flex transition-colors duration-300';
-    const activeClass = isActive ? 'bg-gray-600' : 'bg-transparent';
-
-    return `${activeClass} ${defaultNavLinkClass}`;
-  };
-
   return (
-    <header className="bg-black-background-color px-3 py-1 w-full flex items-center justify-between min-h-[44px] sticky z-10 top-0">
-      <h1 className="my-2 text-xl 2xl:text-4xl font-bold text-white-text-color block">Catatan Pelupa</h1>
+    <header className="bg-white-background-color dark:bg-black-background-color semiDark:bg-black-background-color px-3 py-1 w-full flex items-center justify-between min-h-[44px] sticky z-10 top-0">
+      <h1 className="my-2 text-xl 2xl:text-4xl font-bold text-gray-text-color semiDark:text-white-text-color dark:text-white-text-color block">Catatan Pelupa</h1>
       <div className="flex gap-3 relative">
         <nav className="flex gap-3">
-          <NavLink className={({ isActive }) => NavLinkActiveClass(isActive)} to={HOME} end>
-            <PushPin className="text-3xl text-white-text-color m-auto" weight="light" />
-          </NavLink>
-          <NavLink className={({ isActive }) => NavLinkActiveClass(isActive)} to={ARCHIVE}>
-            <Archive className="text-3xl text-white-text-color m-auto" />
-          </NavLink>
+          <NavLinkButton to={HOME} end>
+            <PushPin className="text-3xl block m-auto" weight="light" />
+          </NavLinkButton>
+          <NavLinkButton to={ARCHIVE}>
+            <Archive className="text-3xl block m-auto" weight="light" />
+          </NavLinkButton>
         </nav>
         <ThemeButton onClick={themeMenuHandler} />
         <ThemeMenuContainer
