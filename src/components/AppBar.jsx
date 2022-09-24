506a686283bb32fc9a73c4eccdf1c17d1c6587dd
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 14:06:16 +0800

    make app title adapt to locale

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -5,9 +5,13 @@ import ThemeMenuButton from './buttons/ThemeMenuButton';
 import ThemeMenuContainer from './ThemeMenuContainer';
 import NavLinkButton from './buttons/NavLinkButton';
 import LocaleToggleButton from './buttons/LocaleToggleButton';
+import LocaleContext from '../contexts/LocaleContext';
 
 function AppBar() {
   const [isThemeMenuDisplayed, setIsThemeMenuDisplayed] = React.useState(false);
+  const { locale } = React.useContext(LocaleContext);
+
+  const appTitle = locale === 'id' ? 'Catatan Pelupa' : 'Forgetful Notes';
 
   const themeMenuToggler = () => {
     setIsThemeMenuDisplayed((prevState) => !prevState);
@@ -19,7 +23,7 @@ function AppBar() {
 
   return (
     <header className="bg-white-background-color dark:bg-black-background-color semi-dark:bg-black-background-color px-3 py-1 w-full flex items-center justify-between min-h-[44px] sticky z-10 top-0">
-      <h1 className="my-2 text-xl 2xl:text-4xl font-bold text-gray-text-color semi-and-dark:text-white-text-color block">Catatan Pelupa</h1>
+      <h1 className="my-2 text-lg 2xl:text-4xl font-bold text-gray-text-color semi-and-dark:text-white-text-color block">{appTitle}</h1>
       <div className="flex gap-1 relative">
         <nav className="flex gap-1">
           <NavLinkButton to={HOME} end>
