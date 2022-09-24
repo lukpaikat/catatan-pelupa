6baca9d923f8f4b0c3b9d64623e89b16d2590711
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 09:16:19 +0800

    style add NewNotePage dark mode

--- src/components/NotePaper.jsx
+++ src/components/NotePaper.jsx
@@ -6,7 +6,7 @@ function NotePaper({ children, noteTitle }) {
   return (
     <section
       className={`py-2 px-6 w-full max-w-[700px] lg:max-w-[1000px] 2xl:max-w-[1300px]
-      ${getNoteColorClassName(noteTitle) || 'bg-orange-note-color'} 
+      ${getNoteColorClassName(noteTitle) || 'bg-orange-note-color dark:bg-orange-dark-note-color'} 
       mx-auto rounded-lg transition-all duration-150 min-h-[92vh] h-fit`}
     >
       {children}
