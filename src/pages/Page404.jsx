7ca012c2b6cc5cb805184afe7b1e114803fd5625
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 17:39:32 +0800

    make 404 page description translateable

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import pageNotFoundIllustratioGreen from '../assets/undraw_page_not_found_green.svg';
 import pageNotFoundIllustrationBlack from '../assets/undraw_page_not_found_black.svg';
 import ThemeContext from '../contexts/ThemeContext';
+import LocaleContext from '../contexts/LocaleContext';
 
 function Page404() {
   const { theme } = React.useContext(ThemeContext);
+  const { locale } = React.useContext(LocaleContext);
 
+  const pageDescription = locale === 'id' ? 'Eeeeits! Halaman yang kamu cari tidak ditemukan.' : 'Oops! We couldn\'t find The page that you are looking for.';
   const pageNotFoundIllustration = theme === 'light' ? pageNotFoundIllustrationBlack : pageNotFoundIllustratioGreen;
 
   return (
     <>
       <img className="w-[80%] max-w-sm md:max-w-md mx-auto mt-[10vh] animate__animated animate__fadeIn" src={pageNotFoundIllustration} alt="404" />
-      {/* TODO: translate ini */}
-      <p className="text-gray-text-color semi-and-dark:text-white-text-color text-xl text-center mt-7 mb-[20vh]">Halaman yang kamu cari tidak ditemukan</p>
+      <p className="text-gray-text-color semi-and-dark:text-white-text-color text-xl text-center mt-7 mb-[20vh]">{pageDescription}</p>
     </>
   );
 }
