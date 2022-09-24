e8acb1d004790806a82c92987e4b9f48b96c9969
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 14:54:30 +0800

    make searchbox placeholder and sr translateable

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ClearSearchButton from './buttons/ClearSearchButton';
+import LocaleContext from '../contexts/LocaleContext';
 
 function SearchBox({ keyword, keywordChange, clearKeyword }) {
+  const { locale } = React.useContext(LocaleContext);
+  const searchBoxSR = locale === 'id' ? 'kotak pencarian' : 'search box';
+  const placeholder = locale === 'id' ? 'cari' : 'search';
+
   return (
     <div className="flex items-center w-full justify-end mb-6">
       <label htmlFor="searchBox" className="w-full">
-        <span className="sr-only">SearchBox</span>
+        <span className="sr-only">{searchBoxSR}</span>
         <input
           type="text"
           id="searchBox"
-          placeholder="cari"
+          placeholder={placeholder}
           onChange={(event) => keywordChange(event.target.value)}
           value={keyword}
           className="text-gray-text-color semi-and-dark:text-white-text-color 2xl:text-lg block w-full bg-gray-200 semi-and-dark:bg-gray-700 min-h-[44px] pl-2 rounded-l-lg semi-and-dark:bg-opacity-80"
