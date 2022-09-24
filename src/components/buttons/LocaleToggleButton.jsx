f71c936a38da3c1adaa83d1ef024b7e009b59999
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 13:55:28 +0800

    remove unused class: flex-col

--- src/components/buttons/LocaleToggleButton.jsx
+++ src/components/buttons/LocaleToggleButton.jsx
@@ -5,7 +5,7 @@ function LocaleToggleButton() {
   const { locale, localeToggle } = React.useContext(LocaleContext);
   const localeUpperCase = locale.toUpperCase();
   return (
-    <button className="app-bar-button-simplified flex-col" type="button" onClick={localeToggle}>
+    <button className="app-bar-button-simplified" type="button" onClick={localeToggle}>
       {localeUpperCase}
     </button>
   );
