5677951164000dd602b345bf0625bbb94f7a3cd9
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 08:47:57 +0800

    style add light hover X icon

--- src/components/buttons/CloseSearchButton.jsx
+++ src/components/buttons/CloseSearchButton.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 function CloseSearchButton({ onClick }) {
   return (
     <button type="button" onClick={onClick} className="bg-gray-200 semi-and-dark:bg-gray-600 pr-1 rounded-r-lg min-h-[44px]">
-      <X size={32} className="text-gray-text-color semi-and-dark:text-white-text-color" />
+      <X size={32} className="text-gray-text-color semi-and-dark:text-white-text-color light:hover:text-black-text-color" />
     </button>
   );
 }
