9463baa3059b8b308bf787f85bf2514ee8797cf0
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-24 17:32:25 +0800

    add todo localeToggle

--- src/components/buttons/LocaleToggleButton.jsx
+++ src/components/buttons/LocaleToggleButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import LocaleContext from '../../contexts/LocaleContext';
-
+// TODO: cek nanti, rada bingung labelnya pakai bahasa inggris atau indo
 function LocaleToggleButton() {
   const { locale, localeToggle } = React.useContext(LocaleContext);
   const localeUpperCase = locale.toUpperCase();
+  const label = locale === 'id' ? 'tombol ganti bahasa' : 'language toggle';
   return (
-    <button className="app-bar-button-simplified" type="button" onClick={localeToggle}>
+    <button aria-label={label} className="app-bar-button-simplified" type="button" onClick={localeToggle}>
       {localeUpperCase}
     </button>
   );
