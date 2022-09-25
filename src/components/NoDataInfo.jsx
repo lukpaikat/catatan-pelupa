5d9ff0d5d3bd6822a93fe7ae2303ec4c8a0ccdc1
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 16:17:20 +0800

    chore(NoDataInfo): move text source to dictionary

--- src/components/NoDataInfo.jsx
+++ src/components/NoDataInfo.jsx
@@ -3,17 +3,18 @@ import ThemeContext from '../contexts/ThemeContext';
 import LocaleContext from '../contexts/LocaleContext';
 import noDataIllustrationLight from '../assets/undraw_no_data_light.svg';
 import noDataIllustrationDark from '../assets/undraw_no_data_dark.svg';
+import dictionary from '../languages/dictionary';
 
 function NoDataInfo() {
   const { locale } = React.useContext(LocaleContext);
   const { theme } = React.useContext(ThemeContext);
   const noDataIllustration = theme === 'light' ? noDataIllustrationLight : noDataIllustrationDark;
-  const description = locale === 'id' ? 'Tidak ada catan' : 'No notes found';
+
   return (
     <>
       <img className="w-[80%] max-w-[120px] xl:max-w-[220px] mx-auto mt-[10vh] animate__animated animate__headShake" src={noDataIllustration} alt="no data" />
       <p className="text-center text-gray-text-color semi-and-dark:text-white-text-color mt-5 2xl:text-lg">
-        {description}
+        {dictionary[locale].noDataDescription}
       </p>
     </>
   );
