2e260b4d2cf2c9c01b4958605bd0757d955fa85d
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 11:54:01 +0800

    add todo for buttons

--- src/components/buttons/ActionButtonAdd.jsx
+++ src/components/buttons/ActionButtonAdd.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import { Plus } from 'phosphor-react';
 
 function ActionButtonAdd({ onClick }) {
+  // TODO: ganti bahasa title
+  // TODO: pakai title untuk nama tombol
   return (
     <button
-      aria-label="create new note"
+      title="create new note"
       type="button"
       onClick={onClick}
       className="h-[44px] w-[44px] drop-shadow-lg rounded-lg active:translate-y-1 active:drop-shadow-md transition-colors duration-300 bg-gray-600 hover:bg-gray-400 focus:bg-gray-400 active:bg-gray-500"
