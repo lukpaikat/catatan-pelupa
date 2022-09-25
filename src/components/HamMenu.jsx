64574acdc0e5ff7593bff5228663457b74869fc3
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 13:34:21 +0800

    create temp HamMenu

--- src/components/HamMenu.jsx
+++ src/components/HamMenu.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { PushPin, Archive } from 'phosphor-react';
+import {
+  PushPin, Archive, Translate, SignOut,
+} from 'phosphor-react';
 import HamItemNavLink from './buttons/HamItemNavLink';
 import { HOME, ARCHIVE } from '../config/paths';
+import HamItemButton from './buttons/HamItemButton';
+import LocaleContext from '../contexts/LocaleContext';
+import dictionary from '../languages/dictionary';
 
 function HamMenu() {
+  const { locale, localeToggle } = React.useContext(LocaleContext);
+
   return (
     <div className="absolute top-full right-0 mt-2
     bg-white-background-color semi-and-dark:bg-gray-700
@@ -14,23 +21,39 @@ function HamMenu() {
         <br />
         User name
       </h1>
-      <ul className="flex flex-col transition-all p-1">
+      <ul className="flex gap-1 flex-col transition-all p-1">
         <li>
           <HamItemNavLink
             to={HOME}
-            aria-label="catatan aktif"
+            aria-label={dictionary[locale].activeNotes}
             end
-            title="Catatan Aktif"
+            title={dictionary[locale].activeNotes}
             icon={<PushPin />}
           />
         </li>
         <li>
           <HamItemNavLink
             to={ARCHIVE}
-            aria-label="catatan arsip"
+            aria-label={dictionary[locale].archiveNotes}
             end
-            title="Catatan arsip"
+            title={dictionary[locale].archiveNotes}
             icon={<Archive />}
+          />
+        </li>
+        <div className="h-px bg-gray-text-color opacity-20 mx-4" />
+        <li>
+          <HamItemButton
+            icon={<Translate />}
+            title={dictionary[locale].toggleLanguage}
+            onClick={localeToggle}
+          />
+        </li>
+        <div className="h-px bg-gray-text-color opacity-20 mx-4" />
+        <li>
+          <HamItemButton
+            icon={<SignOut />}
+            title={dictionary[locale].logOut}
+            onClick={() => console.log('test')}
           />
         </li>
       </ul>
