ed75cacac8dc1854edae8d3385127df481cd623b
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 16:02:10 +0800

    feat: add translation add HamMenu

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -6,25 +6,40 @@ import NavLinkArchive from './buttons/NavLinkArchive';
 import LocaleToggleButton from './buttons/LocaleToggleButton';
 import LocaleContext from '../contexts/LocaleContext';
 import NavLinkPushPin from './buttons/NavLinkPushPin';
+import HamMenu from './HamMenu';
+import dictionary from '../languages/dictionary';
+import HamMenuButton from './buttons/HamMenuButton';
 
 function AppBar() {
   // TODO: tambahkan drawer menu untuk mobile sampai 1024px
   const [isThemeMenuDisplayed, setIsThemeMenuDisplayed] = React.useState(false);
+  const [isHamMenuDisplayed, setIsHamMenuDisplayed] = React.useState(false);
+
   const { locale } = React.useContext(LocaleContext);
 
-  const appTitle = locale === 'id' ? 'Catatan Pelupa' : 'Forgetful Notes';
-
-  const themeMenuToggler = () => {
-    setIsThemeMenuDisplayed((prevState) => !prevState);
-  };
-
-  const menuOutsideClick = () => {
+  const themeMenuHider = () => {
     setIsThemeMenuDisplayed(() => false);
   };
 
+  const hamMenuHider = () => {
+    setIsHamMenuDisplayed(() => false);
+  };
+
+  const themeMenuToggler = () => {
+    setIsThemeMenuDisplayed((prevState) => !prevState);
+    hamMenuHider();
+  };
+
+  const hamMenuToggler = () => {
+    setIsHamMenuDisplayed((prevState) => !prevState);
+    themeMenuHider();
+  };
+
+  // TODO: buat tombol hamburger
+
   return (
     <header className="bg-white-background-color dark:bg-black-background-color semi-dark:bg-black-background-color px-3 py-1 w-full flex items-center justify-between min-h-[44px] sticky z-10 top-0">
-      <h1 className="my-2 text-lg 2xl:text-4xl font-bold text-gray-text-color semi-and-dark:text-white-text-color block">{appTitle}</h1>
+      <h1 className="my-2 text-lg 2xl:text-4xl font-bold text-gray-text-color semi-and-dark:text-white-text-color block">{dictionary[locale].appTitle}</h1>
       <div className="flex gap-1 relative">
         <nav className="flex gap-1">
           <NavLinkPushPin to={HOME} end />
@@ -34,10 +49,12 @@ function AppBar() {
         <LocaleToggleButton />
         <ThemeMenuButton onClick={themeMenuToggler} />
         <ThemeMenuContainer
-          menuOutsideClick={() => menuOutsideClick()}
+          themeMenuHider={() => themeMenuHider()}
           themeMenuToggler={themeMenuToggler}
           isThemeMenuDisplayed={isThemeMenuDisplayed}
         />
+        <HamMenuButton onClick={hamMenuToggler} />
+        <HamMenu isDisplayed={isHamMenuDisplayed} hamMenuHider={hamMenuHider} />
       </div>
     </header>
   );
