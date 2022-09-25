35a0b4eb8defdce6206c79935601562c519eb4fb
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 18:02:00 +0800

    add switch transition to ham menu icon

--- src/components/buttons/HamMenuButton.jsx
+++ src/components/buttons/HamMenuButton.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import { List, X } from 'phosphor-react';
 
 function HamMenuButton({ onClick, isHamMenuDisplayed }) {
+  const xIconRef = React.useRef(null);
+  const listIconRef = React.useRef(null);
+  const nodeRef = isHamMenuDisplayed ? xIconRef : listIconRef;
   return (
+
     <button
       aria-expanded={isHamMenuDisplayed}
       title="Menu"
@@ -14,18 +19,31 @@ function HamMenuButton({ onClick, isHamMenuDisplayed }) {
         onClick();
       }}
     >
-      {isHamMenuDisplayed
-        ? (
-          <X
-            weight="light"
-            className="text-3xl 2xl:text-5xl text-gray-text-color hover:text-black-text-color semi-and-dark:text-white-text-color m-auto"
-          />
-        ) : (
-          <List
-            weight="light"
-            className="text-3xl 2xl:text-5xl text-gray-text-color hover:text-black-text-color semi-and-dark:text-white-text-color m-auto"
-          />
-        )}
+      <SwitchTransition>
+        <CSSTransition
+          key={isHamMenuDisplayed}
+          nodeRef={nodeRef}
+          addEndListener={(done) => {
+            nodeRef.current.addEventListener('transitionend', done, false);
+          }}
+          classNames="fade"
+        >
+          <div ref={nodeRef}>
+            {isHamMenuDisplayed
+              ? (
+                <X
+                  weight="light"
+                  className="text-3xl 2xl:text-5xl text-gray-text-color hover:text-black-text-color semi-and-dark:text-white-text-color m-auto"
+                />
+              ) : (
+                <List
+                  weight="light"
+                  className="text-3xl 2xl:text-5xl text-gray-text-color hover:text-black-text-color semi-and-dark:text-white-text-color m-auto"
+                />
+              )}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </button>
   );
 }
