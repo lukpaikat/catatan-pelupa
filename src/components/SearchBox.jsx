29987cbd0c0c1a52bcf0e04734921ca7556a99b4
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 16:16:32 +0800

    chore(searchbox): move language source to dic

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ClearSearchButton from './buttons/ClearSearchButton';
 import LocaleContext from '../contexts/LocaleContext';
+import dictionary from '../languages/dictionary';
 
 function SearchBox({ keyword, keywordChange, clearKeyword }) {
   const { locale } = React.useContext(LocaleContext);
-  const searchBoxSR = locale === 'id' ? 'kotak pencarian' : 'search box';
-  const placeholder = locale === 'id' ? 'cari' : 'search';
 
   return (
     <div className="flex items-center w-full justify-end mb-6">
       <label htmlFor="searchBox" className="w-full">
-        <span className="sr-only">{searchBoxSR}</span>
+        <span className="sr-only">{dictionary[locale].searchBoxSR}</span>
         <input
           type="text"
           id="searchBox"
-          placeholder={placeholder}
+          placeholder={dictionary[locale].searchPlaceholder}
           onChange={(event) => keywordChange(event.target.value)}
           value={keyword}
           className="text-gray-text-color semi-and-dark:text-white-text-color 2xl:text-lg block w-full bg-gray-200 semi-and-dark:bg-gray-700 min-h-[44px] pl-2 rounded-l-lg semi-and-dark:bg-opacity-80"
