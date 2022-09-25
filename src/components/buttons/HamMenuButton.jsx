9f53aa472c4ae8730e29f33450ee5591f72d5b9d
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 17:29:19 +0800

    update HamMenuButton Language add X icon

--- src/components/buttons/HamMenuButton.jsx
+++ src/components/buttons/HamMenuButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { List } from 'phosphor-react';
+import { List, X } from 'phosphor-react';
 
-function HamMenuButton({ onClick }) {
+function HamMenuButton({ onClick, isHamMenuDisplayed }) {
   return (
     <button
+      aria-expanded={isHamMenuDisplayed}
       title="Menu"
       type="button"
       className="app-bar-button-simplified"
@@ -13,15 +14,24 @@ function HamMenuButton({ onClick }) {
         onClick();
       }}
     >
-      <List
-        weight="light"
-        className="text-3xl 2xl:text-5xl text-gray-text-color hover:text-black-text-color semi-and-dark:text-white-text-color m-auto"
-      />
+      {isHamMenuDisplayed
+        ? (
+          <X
+            weight="light"
+            className="text-3xl 2xl:text-5xl text-gray-text-color hover:text-black-text-color semi-and-dark:text-white-text-color m-auto"
+          />
+        ) : (
+          <List
+            weight="light"
+            className="text-3xl 2xl:text-5xl text-gray-text-color hover:text-black-text-color semi-and-dark:text-white-text-color m-auto"
+          />
+        )}
     </button>
   );
 }
 
 HamMenuButton.propTypes = {
+  isHamMenuDisplayed: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
