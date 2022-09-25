d82249fa4f01bddea5269a39099a0d9bf524c96f
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-25 16:35:31 +0800

    add translateable ClearSearchButton title

--- src/components/buttons/ClearSearchButton.jsx
+++ src/components/buttons/ClearSearchButton.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { X } from 'phosphor-react';
 import PropTypes from 'prop-types';
+import LocaleContext from '../../contexts/LocaleContext';
+import dictionary from '../../languages/dictionary';
 
 function ClearSearchButton({ onClick }) {
+  const { locale } = React.useContext(LocaleContext);
+  const title = dictionary[locale].clearSearchButton;
+
   return (
-    <button type="button" onClick={onClick} className="bg-gray-200 semi-and-dark:bg-gray-700 semi-and-dark:bg-opacity-80 pr-1 rounded-r-lg min-h-[44px]">
+    <button
+      title={title}
+      type="button"
+      onClick={onClick}
+      className="bg-gray-200 semi-and-dark:bg-gray-700 semi-and-dark:bg-opacity-80 pr-1 rounded-r-lg min-h-[44px]"
+    >
       <X size={32} className="text-gray-text-color semi-and-dark:text-white-text-color light:hover:text-black-text-color" />
     </button>
   );
