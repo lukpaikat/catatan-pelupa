302b8977f2303194e101263b7ea694b095e0b76b
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-26 17:58:31 +0800

    use blue container on LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LoginForm from '../components/LoginForm';
 import { login } from '../utils/network-data';
+import BlueContainer from '../components/BlueContainer';
 
 function LoginPage({ onloginSuccess }) {
-  const handleLogin = async ({ email, password }) => {
-    const { error, data } = await login({ email, password });
+  const handleLogin = async (user) => {
+    const { error, data } = await login(user);
 
     if (!error) {
       onloginSuccess(data.accessToken);
     }
   };
   return (
-    <div className="flex justify-center items-center">
+    <BlueContainer>
       <LoginForm handleLogin={handleLogin} />
-    </div>
+    </BlueContainer>
   );
 }
 
