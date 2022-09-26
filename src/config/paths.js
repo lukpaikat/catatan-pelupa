b505af903b6a646bee7fc6633af0573d76759ed0
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-26 11:30:03 +0800

    add LOGIN and REGISTER path

--- src/config/paths.js
+++ src/config/paths.js
@@ -3,6 +3,8 @@ const ARCHIVE = '/archive';
 const NOTES_NEW = '/notes/new';
 const NOTES_DETAIL = '/notes/'; // add id; /notes/:id
 const PAGE404 = '/404page';
+const LOGIN = '/login';
+const REGISTER = '/register';
 
 export {
   HOME,
@@ -10,4 +12,6 @@ export {
   NOTES_NEW,
   NOTES_DETAIL,
   PAGE404,
+  LOGIN,
+  REGISTER,
 };
