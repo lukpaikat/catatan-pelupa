a668b1e39e99f656a2ccdae5369026f6519e6288
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-26 14:31:49 +0800

    style: temp style login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -13,11 +13,11 @@ function LoginForm({ handleLogin }) {
   };
 
   return (
-    <form className="flex flex-col gap-4" onSubmit={handleOnSubmit}>
+    <form className="flex flex-col gap-4 bg-sky-500 p-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" onSubmit={handleOnSubmit}>
       <h1 className="semi-and-dark:text-white-text-color">Halaman Login</h1>
       <input title="email" type="email" value={email} onChange={setEmail} />
       <input title="password" type="password" value={password} onChange={setPassword} />
-      <button type="submit">Masuk</button>
+      <button className="px-4 py-2 bg-orange-300" type="submit">Masuk</button>
     </form>
   );
 }
