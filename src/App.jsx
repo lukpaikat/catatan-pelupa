2ac6971334faf223665778b925134f60594818b5
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-26 14:32:33 +0800

    add login functionality remove rerouting when 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
 import {
-  HOME, ARCHIVE, NOTES_NEW, NOTES_DETAIL, PAGE404,
+  Route, Routes,
+} from 'react-router-dom';
+import {
+  HOME, ARCHIVE, NOTES_NEW, NOTES_DETAIL, REGISTER,
 } from './config/paths';
+// Pages
+import LoginPage from './pages/LoginPage';
 import AppBar from './components/AppBar';
 import HomePage from './pages/HomePage';
 import ArchivePage from './pages/ArchivePage';
 import Page404 from './pages/Page404';
 import NewNotePage from './pages/NewNotePage';
 import NoteDetailPage from './pages/NoteDetailPage';
+// Contexts
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LocaleProvider } from './contexts/LocaleContext';
+// css
 import 'animate.css';
-// TODO: buat app bisa berganti bahasa
+import { getUserLogged, putAccessToken } from './utils/network-data';
+
 function App() {
+  const [initializing, setInitializing] = React.useState(true);
+  const [authedUser, setAuthedUser] = React.useState(null);
   const [locale, setLocale] = React.useState(localStorage.getItem('locale') || 'id');
   const [theme, setTheme] = React.useState(localStorage.getItem('theme') || 'semiDark');
+
+  React.useEffect(() => {
+    const initiateUser = async () => {
+      const { data } = await getUserLogged('app executed this');
+      setAuthedUser(() => data);
+      setInitializing(() => false);
+    };
+    initiateUser();
+  }, []);
 
   const localeToggle = () => {
     setLocale((prevState) => {
@@ -45,18 +63,46 @@ function App() {
     changeTheme,
   }), [theme]);
 
+  const onLoginSuccess = async (accessToken) => {
+    putAccessToken(accessToken);
+    const { data } = await getUserLogged();
+    setAuthedUser(() => data);
+  };
+
+  if (initializing) {
+    return null;
+  }
+
+  if (authedUser === null) {
+    return (
+      <LocaleProvider value={localeContextValue}>
+        <ThemeProvider value={themeContextValue}>
+          <AppBar />
+          <main className="flex flex-col h-full">
+            <Routes>
+              <Route
+                path="*"
+                element={<LoginPage onloginSuccess={onLoginSuccess} />}
+              />
+              <Route path={REGISTER} element={<p>Halaman register</p>} />
+            </Routes>
+          </main>
+        </ThemeProvider>
+      </LocaleProvider>
+    );
+  }
+
   return (
     <LocaleProvider value={localeContextValue}>
       <ThemeProvider value={themeContextValue}>
-        <AppBar />
+        <AppBar authedUser={authedUser} />
         <main className="px-2">
           <Routes>
             <Route path={HOME} element={<HomePage />} />
             <Route path={ARCHIVE} element={<ArchivePage />} />
             <Route path={NOTES_NEW} element={<NewNotePage />} />
             <Route path={`${NOTES_DETAIL}:id`} element={<NoteDetailPage />} />
-            <Route path={PAGE404} element={<Page404 />} />
-            <Route path="*" element={<Navigate to={PAGE404} />} />
+            <Route path="*" element={<Page404 />} />
           </Routes>
         </main>
       </ThemeProvider>
