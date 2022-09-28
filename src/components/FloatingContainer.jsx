2c73eb2cf90da90217cf48b689eae3a2fe186930
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-29 07:05:54 +0800

    style(positioning): make it symetrical with toast

--- src/components/FloatingContainer.jsx
+++ src/components/FloatingContainer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function FloatingContainer({ children }) {
   return (
-    <div className="flex gap-2 fixed bottom-8 right-8">
+    <div className="flex gap-2 fixed bottom-4 right-4 sm:bottom-8 sm:right-8 sm:p-1">
       {children}
     </div>
   );
