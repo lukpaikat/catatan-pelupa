2d759a5f3a7c0db3c42f92ecfd309371c9b21adb
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-28 11:22:58 +0800

    style: registration page

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import RegistrationForm from '../components/RegistrationForm';
 import { register } from '../utils/network-data';
+import ThemeContext from '../contexts/ThemeContext';
+import LocaleContext from '../contexts/LocaleContext';
+import dictionary from '../languages/dictionary';
+import { LOGIN } from '../config/paths';
+import { ReactComponent as CatsDark } from '../assets/happy_cat_dark.svg';
+import { ReactComponent as CatsLight } from '../assets/happy_cat_light.svg';
 
 function RegistrationPage() {
   const [isRegistering, setIsRegistering] = React.useState(false);
+  const { locale } = React.useContext(LocaleContext);
+  const { theme } = React.useContext(ThemeContext);
+  const { welcome, haveAccount, logInHere } = dictionary[locale];
   const navigate = useNavigate();
   const handleRegister = async (user) => {
     setIsRegistering(() => true);
@@ -20,10 +29,28 @@ function RegistrationPage() {
   };
 
   return (
-    <div className="flex flex-col gap-4 bg-sky-200 semi-dark:bg-blue-note-color dark:bg-gray-700
-    p-10 m-auto rounded-lg w-[90vw] max-w-3xl shadow-md"
-    >
-      <RegistrationForm handleRegister={handleRegister} isRegistering={isRegistering} />
+    <div className="h-[80vh] min-h-fit flex flex-col pt-8">
+      <div className={`flex flex-col gap-4 bg-sky-200 semi-dark:bg-blue-note-color dark:bg-gray-700
+        m-auto rounded-lg w-[90vw] max-w-3xl shadow-md pause animate-cat ${isRegistering && 'play'}`}
+      >
+        <div className="p-4 mx-auto my-16">
+          { theme === 'dark'
+            ? <CatsLight className="w-[80%] mx-auto px-5 h-fit overflow-visible" />
+            : <CatsDark className="w-[80%] mx-auto px-5 h-fit overflow-visible" />}
+          <h1
+            className="uppercase font-lato text-3xl sm:text-6xl drop-shadow-md mt-4
+          text-center text-gray-text-color dark:text-white-text-color"
+          >
+            {welcome}
+          </h1>
+          <RegistrationForm handleRegister={handleRegister} isRegistering={isRegistering} />
+          <p className="text-gray-text-color dark:text-white-text-color text-center my-4">
+            {haveAccount}
+            {' '}
+            <Link className="text-orange-600 dark:text-orange-400 underline" to={LOGIN}>{logInHere}</Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
