450085cd310dc6fea1ad3e734047615c4eca00c9
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-28 10:37:23 +0800

    feat: add registering state

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -4,8 +4,10 @@ import RegistrationForm from '../components/RegistrationForm';
 import { register } from '../utils/network-data';
 
 function RegistrationPage() {
+  const [isRegistering, setIsRegistering] = React.useState(false);
   const navigate = useNavigate();
   const handleRegister = async (user) => {
+    setIsRegistering(() => true);
     const { error } = await register(user);
     if (!error) {
       navigate('/');
@@ -14,13 +16,14 @@ function RegistrationPage() {
       // eslint-disable-next-line no-alert
       alert('Registration success, please try to login');
     }
+    setIsRegistering(() => false);
   };
 
   return (
     <div className="flex flex-col gap-4 bg-sky-200 semi-dark:bg-blue-note-color dark:bg-gray-700
     p-10 m-auto rounded-lg w-[90vw] max-w-3xl shadow-md"
     >
-      <RegistrationForm handleRegister={handleRegister} />
+      <RegistrationForm handleRegister={handleRegister} isRegistering={isRegistering} />
     </div>
   );
 }
