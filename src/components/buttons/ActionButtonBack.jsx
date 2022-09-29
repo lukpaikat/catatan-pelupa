874ad69231d3a3d89dba2a3d857893ac02564a72
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-29 08:55:20 +0800

    fix wrong import for icon and Link

--- src/components/buttons/ActionButtonBack.jsx
+++ src/components/buttons/ActionButtonBack.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ArrowLeft } from 'react-router-dom';
-import { Plus } from 'phosphor-react';
+import { Link } from 'react-router-dom';
+import { ArrowLeft } from 'phosphor-react';
 import LocaleContext from '../../contexts/LocaleContext';
 import dictionary from '../../languages/dictionary';
 
@@ -10,13 +10,13 @@ function ActionButtonBack({ to }) {
   const { back } = dictionary[locale];
 
   return (
-    <ArrowLeft
+    <Link
       to={to}
       title={back}
       className="h-[44px] w-[44px] drop-shadow-lg rounded-lg active:translate-y-1 active:drop-shadow-md transition-colors duration-300 bg-gray-600 hover:bg-gray-400 focus:bg-gray-400 active:bg-gray-500"
     >
-      <Plus className="m-auto text-3xl text-white-text-color" weight="light" />
-    </ArrowLeft>
+      <ArrowLeft className="m-auto text-3xl text-white-text-color" weight="light" />
+    </Link>
   );
 }
 
