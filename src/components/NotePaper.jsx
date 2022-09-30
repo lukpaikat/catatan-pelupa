f7681e8ffc0510fcccd181c86df8382881f8813c
lukpaikat <mhasbisaputra5@gmail.com>
2022-09-30 13:35:46 +0800

    add more padding for notes paper

--- src/components/NotePaper.jsx
+++ src/components/NotePaper.jsx
@@ -5,7 +5,7 @@ import getNoteColorClassName from '../utils/getNoteColorClassName';
 function NotePaper({ children, noteTitle }) {
   return (
     <section
-      className={`py-2 px-6 w-full max-w-[700px] lg:max-w-[1000px] 2xl:max-w-[1300px]
+      className={`p-6 w-full max-w-[700px] lg:max-w-[1000px] 2xl:max-w-[1300px]
       ${getNoteColorClassName(noteTitle) || 'bg-orange-note-color dark:bg-orange-dark-note-color'} 
       mx-auto my-4 rounded-lg transition-all duration-150 min-h-[45vh] h-fit`}
     >
